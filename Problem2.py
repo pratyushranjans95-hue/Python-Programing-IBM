9e4c9470fab1d8d32ea27352a0ca6cd6c446cd82
Pratyush Ranjan <pratyushranjans95@gmail.com>
2026-09-24 15:35:20 +0530

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,3 @@
+# Wap to Take name as input and greet the user 
+a = input("enter the namae")
+print(f"Hello {a}, Have a nice day")
---
+++ Problem20.py
@@ -0,0 +1,12 @@
+m = int(input("Enter The Total Marks"))
+
+if 100<=m<90:
+    print("A Grade!")
+elif 90<=m<70:
+    print("B Grade!")
+elif 70<=m<50:
+    print("C Grade!")
+elif 50<=m<30:
+    print("D Grade!")
+else:
+    print("Fail!")                 
